59decf29d875eebabad21d0a8aeac75a4d6e9dae
Sean Charles <objitsu@gmail.com>
2021-11-19 21:03:09 +0000

    Some updates and changes

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ GENEXT=d,o,mh,err,c,c_date,mh,mih
 GRADE=hlc.gc
 # this one links to the extras folder in case you need it.
 # FLAGS=--ml posix --mld /usr/local/mercury-rotd-2021-04-15/extras/lib/mercury -s $(GRADE) -O4
-FLAGS=-s $(GRADE) -O4
+FLAGS=-s $(GRADE) -O4 -E
 
 all:: $(BIN)
 
--- mcnew.m
+++ mcnew.m
@@ -1,67 +1,147 @@
-%--------------------------------------------------%
-%--------------------------------------------------%
+%-----------------------------------------------------------------------------%
 %
 %  mcnew - Make a new stub mercury main() program.
 %
-% 
-%--------------------------------------------------%
-%--------------------------------------------------%
+% My first 'real' Mercury written as an apprentice piece to start getting
+% some real experience cutting code and getting real flying time.
+%
+% Change History
+% ==============
+% 19 Nov 2021 -- revamped with everything I've learned in the last few months!
+%                * Hopefully more idiomatic code.
+%                * Use of custom command type.
+%                * Improved content of main().
+%                * Makefile tweaks (-E added)
+%
+%-----------------------------------------------------------------------------%
 :- module mcnew.
 :- interface.
 :- import_module io.
-:- pred main(io::di, io::uo) is det.
-:- implementation.
-:- import_module list, string, time.
 
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module list.
+:- import_module maybe.
+:- import_module string.
+:- import_module time.
+
+%-----------------------------------------------------------------------------%
+
+    % MAIN entry point.
+    % This small utility is designed to help make it easier to start using
+    % the Melbourne Mercury Compiler by providing a quick way to generate a
+    % stub program and a Makefile if needed.
+    %
 main(!IO) :-
-    io.command_line_arguments(Args, !IO),
+    set_options(Action, !IO),
     (
-        Args = [Cmd, Name]
-    ->
-        (   if Cmd = "stub" then
-                create_stub(Name, !IO)
-            else if Cmd = "makefile" then
-                create_makefile(Name, !IO)
-            else
-                io.format("Don't know how to do that.\n",[],!IO)
-        )
+        Action = stub(Name, Author),
+        create_stub(Name, Author, !IO)
     ;
+        Action = makefile(Name),
+        create_makefile(Name, !IO)
+    ;
+        Action = usage,
         show_usage(!IO)
     ).
 
+%-----------------------------------------------------------------------------%
 
-:- pred create_stub(string::in, io::di, io::uo) is det.
-create_stub(Name, !IO) :-
+    % Set processing options.
+    % Extract a command and a name, an an optional author name to use
+    % Command will contain the appropriate command line  action and data
+    % to use to complete it.
+    %
+:- type options
+    --->    stub(string, string)    % we want to create a program stub
+    ;       makefile(string)        % we want to create a Makefile
+    ;       usage.                  % confused, show the usage details!
+
+:- pred set_options(options::out, io::di, io::uo) is det.
+
+set_options(Command, !IO) :-
+    io.command_line_arguments(Args, !IO),
+    ( if Args = [Cmd, Name] then
+        get_environment_var("MCNEW_AUTHOR", Ares, !IO),
+        (  Ares = yes(Author)
+        ;  Ares = no, Author = ""
+        ),
+        Command = set_command(Cmd, Name, Author)
+    else if Args = [Cmd, Name, Auth ] then
+        Command = set_command(Cmd, Name, Auth)
+    else
+        Command = usage
+    ).
+
+
+:- func set_command(string::in, string::in, string::in)
+    = (options::out) is det.
+
+set_command(Cmd, Name, Author) = Command :-
+    OpCode = string.to_lower(Cmd),
+    ( if OpCode = "stub" then
+        Command = stub(Name, Author)
+    else if OpCode = "makefile" then
+        Command = makefile(Name)
+    else
+        Command = usage
+    ).
+
+%---------------------------------------------------------------------------%
+
+    % Creates a stub.
+    % Name is the name of the module to create the stub for.
+    % Author contains the initial author name.
+    % !IO is the world state.
+    %
+:- pred create_stub(string::in, string::in, io::di, io::uo) is det.
+create_stub(Name, Author, !IO) :-
     % when did this monumental thing happen ?
     time.time(TNow, !IO),
     io.format("\
-%%---------------------------------------------------------------------------%%
-%%---------------------------------------------------------------------------%%
+%%-----------------------------------------------------------------------------%%
 %%
 %% File: %s.m
-%% Main authors: sjc
+%% Main author: %s
 %% Date: %s%%
 %%
 %% Start...
 %%
-%%---------------------------------------------------------------------------%%
-%%---------------------------------------------------------------------------%%
+%%-----------------------------------------------------------------------------%%
 :- module %s.
+
 :- interface.
 :- import_module io.
+
 :- pred main(io::di, io::uo) is det.
+
 :- implementation.
-:- import_module list, string.
+:- import_module list.
+:- import_module string.
 
 main(!IO) :-
-    .\n",
-    [
-        s(Name),
+    %% ...your great code goes here!
+    io.format(""Mercury demands focus!\n"", [], !IO).
+
+
+%%----------------------------------------------------------------------------%%
+:- end_module %s.
+%%----------------------------------------------------------------------------%%
+
+", [    s(Name), s(Author),
         s(time.asctime(time.gmtime(TNow))),
-        s(Name)
-    ], !IO).
+        s(Name), s(Name)
+   ], !IO).
 
+%---------------------------------------------------------------------------%
 
+    % Create simple Makefile.
+    % Name is the name of the module to create the stub for.
+    % !IO is the world state.
+    % This will write to stdout a simple makefile that enables one to create
+    % the beginnings of a more complex project over time.
+    %
 :- pred create_makefile(string::in, io::di, io::uo) is det.
 
 create_makefile(Name, !IO) :-
@@ -72,8 +152,9 @@ FILES=$(patsubst %%.m,%%,$(DEPS))
 GENEXT=d,o,mh,err,c,c_date,mh,mih
 GRADE=hlc.gc
 # this one links to the extras folder in case you need it.
-# FLAGS=--ml posix --mld /usr/local/mercury-rotd-2021-04-15/extras/lib/mercury -s $(GRADE) -O4
-FLAGS=-s $(GRADE) -O4
+# but note that you may need to change the /usr/local/MERCURY-ROOT/..
+# FLAGS=--ml posix --mld /usr/local/mercury-rotd-2021-04-15/extras/lib/mercury -s $(GRADE) -O4 -E
+FLAGS=-s $(GRADE) -O4 -E
 
 all:: $(BIN)
 
@@ -90,16 +171,29 @@ $(BIN): $(DEPS)
 clean::
 	rm -rf Mercury
 	rm -fv $$(for x in $(FILES); do echo $$x.{$(GENEXT)}; done)
-	rm -fv $(BIN)\n",
-    [s(Name), s(Name)],
+	rm -fv $(BIN)
+",  [s(Name), s(Name)],
     !IO).
 
+%----------------------------------------------------------------------------%
 
+    % Show Usage
+    % Shows the basic program usage information.
+    %
 :- pred show_usage(io::di, io::uo) is det.
 
 show_usage(!IO) :-
-       io.format("usage :- mcnew CMD NAME
+       io.format("\
+usage :- mcnew CMD NAME
 \tCMD   one of `stub` or `makefile`
 \tNAME  the module name minus the .m suffix.
-",
-            [], !IO).
+
+eg. mcnew stub foo > foo.m
+    mcnew stub foo author > foo.m
+    mcnew makefile foo > Makefile
+
+If you don't supply an author, the program will use the value
+of the environment variable MCNEW_AUTHOR, defaulting to the
+empty string if that isn't present either.
+
+", [], !IO).
